22f4b9d3362b17482b94530f96259bedf53c6240
adam <adamjl204@gmail.com>
2023-08-09 23:26:02 +0000

    install codeium

--- install.sh
+++ install.sh
@@ -4,44 +4,16 @@ mkdir -p $HOME/.local/share/code-server/User
 cp keybindings.json $HOME/.local/share/code-server/User/keybindings.json
 cp settings.json $HOME/.local/share/code-server/User/settings.json
 
-myArray=("GitHub.copilot-chat" "GitHub.copilot")
-for p in ${myArray[@]};
-do
-  P="$(cut -d'.' -f1 <<<"$p")"
-  E="$(cut -d'.' -f2 <<<"$p")"
-  DIR="$p"
-  if [ ! -d $DIR ] || [ ! "$(ls -A $DIR)" ]; then
-    echo "fetching $p to $(pwd)"
+install_extension(){
+  publisher="$1"
+  extension_name="$2"
+  version="$3"
+  download_url="https://${publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${publisher}/extension/${extension_name}/${version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
+  wget "$download_url" -O "${extension_name}.VSIX"
+  /tmp/code-server/bin/code-server --install-extension "${extension_name}.VSIX"
+  rm "${extension_name}.VSIX"
+}
 
-    if [ ! -f "$(pwd)/$p.vsix" ]; then
-      curl "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/$P/vsextensions/$E/latest/vspackage" \
-      -H 'Upgrade-Insecure-Requests: 1' \
-      -H 'DNT: 1' \
-      -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' \
-      -H "Referer: https://marketplace.visualstudio.com/items?itemName=$p" \
-      --compressed --output "$(pwd)/$p.vsix"
+# 1.2.63
+install_extension Codeium codeium latest
 
-      sleep 6
-    else
-      echo "File $(pwd)/$p.vsix already exists, skipping download."
-    fi
-  else
-    echo "skipping $p"
-  fi
-done
-
-
-# TODO possible hacky solution to install copilot chat in code-server
-# sudo apt-get install zip
-# unzip GitHub.copilot-chat.vsix -d GitHub.copilot-chat
-
-# replace 1.81.0 with 1.76.0 in source files
-# find GitHub.copilot-chat -type f -exec sed -i 's/1\.81\.0/1.76.0/g' {} +
-
-# zip up the folder
-# cd GitHub.copilot-chat && zip -r ../GitHub.copilot-chat-final.vsix * && cd ..
-
-/tmp/code-server/bin/code-server --install-extension GitHub.copilot.vsix
-
-# code-server --install-extension GitHub.copilot-chat-final.vsix
-# rm -rf ./GitHub.copilot-chat
---
+++ wip-copilot.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+myArray=("GitHub.copilot-chat" "GitHub.copilot")
+for p in ${myArray[@]};
+do
+  P="$(cut -d'.' -f1 <<<"$p")"
+  E="$(cut -d'.' -f2 <<<"$p")"
+  DIR="$p"
+  if [ ! -d $DIR ] || [ ! "$(ls -A $DIR)" ]; then
+    echo "fetching $p to $(pwd)"
+
+    if [ ! -f "$(pwd)/$p.vsix" ]; then
+      curl "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/$P/vsextensions/$E/latest/vspackage" \
+      -H 'Upgrade-Insecure-Requests: 1' \
+      -H 'DNT: 1' \
+      -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' \
+      -H "Referer: https://marketplace.visualstudio.com/items?itemName=$p" \
+      --compressed --output "$(pwd)/$p.vsix"
+
+      sleep 6
+    else
+      echo "File $(pwd)/$p.vsix already exists, skipping download."
+    fi
+  else
+    echo "skipping $p"
+  fi
+done
+
+
+# TODO possible hacky solution to install copilot chat in code-server
+# sudo apt-get install zip
+# unzip GitHub.copilot-chat.vsix -d GitHub.copilot-chat
+
+# replace 1.81.0 with 1.76.0 in source files
+# find GitHub.copilot-chat -type f -exec sed -i 's/1\.81\.0/1.76.0/g' {} +
+
+# zip up the folder
+# cd GitHub.copilot-chat && zip -r ../GitHub.copilot-chat-final.vsix * && cd ..
+
+/tmp/code-server/bin/code-server --install-extension GitHub.copilot.vsix
+
+# code-server --install-extension GitHub.copilot-chat-final.vsix
+# rm -rf ./GitHub.copilot-chat
