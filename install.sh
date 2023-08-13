77c51c5c8dd1f2e76d0b81c791ba4ebbc088e3ae
Adam LaCombe <adamlacombe@users.noreply.github.com>
2023-08-13 18:33:17 -0400

    set git author

--- install.sh
+++ install.sh
@@ -17,3 +17,12 @@ install_extension(){
 # 1.2.63
 install_extension Codeium codeium latest
 
+# set git author
+echo 'export GIT_AUTHOR_NAME="Adam LaCombe"' >> /home/${USER}/.bashrc
+echo 'export GIT_COMMITTER_NAME="Adam LaCombe"' >> /home/${USER}/.bashrc
+echo 'export GIT_AUTHOR_EMAIL="git@adamlacombe.com"' >> /home/${USER}/.bashrc
+echo 'export GIT_COMMITTER_EMAIL="git@adamlacombe.com"' >> /home/${USER}/.bashrc
+source ~/.bashrc
+git config --global user.name "Adam LaCombe"
+git config --global user.email "git@adamlacombe.com"
+
