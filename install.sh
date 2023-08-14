07f6c948cb40518a6e804aea825d1cf42aef6196
Adam LaCombe <adamlacombe@users.noreply.github.com>
2023-08-13 20:40:41 -0400

    use install_vscode_extensions script

--- install.sh
+++ install.sh
@@ -4,18 +4,26 @@ mkdir -p $HOME/.local/share/code-server/User
 cp keybindings.json $HOME/.local/share/code-server/User/keybindings.json
 cp settings.json $HOME/.local/share/code-server/User/settings.json
 
-install_extension(){
-  publisher="$1"
-  extension_name="$2"
-  version="$3"
-  download_url="https://${publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${publisher}/extension/${extension_name}/${version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
-  wget "$download_url" -O "${extension_name}.VSIX"
-  /tmp/code-server/bin/code-server --install-extension "${extension_name}.VSIX"
-  rm "${extension_name}.VSIX"
-}
+# Check if install_vscode_extensions exists
+if ! command -v install_vscode_extensions &> /dev/null; then
+    echo "install_vscode_extensions command not found! Installing..."
+    
+    # Download the script from the gist
+    wget https://gist.github.com/adamlacombe/102d223df7a3d659b5055fd3e0c85c4f/raw/install_vscode_extensions.sh -O /tmp/install_vscode_extensions.sh
+    
+    # Make sure it's executable
+    chmod +x /tmp/install_vscode_extensions.sh
+    
+    # Move it to a directory in the PATH, or you can symlink it as shown before
+    sudo mv /tmp/install_vscode_extensions.sh /usr/local/bin/install_vscode_extensions
 
-# 1.2.63
-install_extension Codeium codeium latest
+    echo "install_vscode_extensions installed successfully!"
+else
+    echo "install_vscode_extensions is already installed."
+fi
+
+# install extensions.json
+install_vscode_extensions
 
 # set git author
 echo 'export GIT_AUTHOR_NAME="Adam LaCombe"' >> /home/${USER}/.bashrc
